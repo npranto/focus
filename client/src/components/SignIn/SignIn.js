ae31dca0fa5bfbca7a7335b6657912bf91a46d63
Focus <Focus@nat-wireless-guest-reg-153-50.bu.edu>
2018-02-05 13:37:01 -0500

    updates layout design for SignIn component

--- client/src/components/SignIn/SignIn.js
+++ client/src/components/SignIn/SignIn.js
@@ -10,16 +10,16 @@ class SignIn extends Component {
 				<div className="container">
 					<h5 className="welcome-back center-align"> Welcome back! </h5>
 					<div className="sign-in-form-container row">
-						<form className="local-sign-in col s12 m6 l6 offset-m3 offset-l3 blue-grey lighten-5">
+						<form className="local-sign-in col s12 m6 l6 offset-m3 offset-l3 grey lighten-3">
 							<div className="row">
 								<div className="input-field col s12 m12 l12">
-									<input placeholder="jsmith@gmail.com" id="email" type="text" className="validate"/>
+									<input placeholder="" id="email" type="text" className="validate"/>
 									<label htmlFor="email">Email</label>
 						        </div>
 							</div>
 							<div className="row">
 								<div className="input-field col s12 m12 l12">
-									<input placeholder="******" id="password" type="password" className="validate"/>
+									<input placeholder="" id="password" type="password" className="validate"/>
 									<label htmlFor="password">Password</label>
 						        </div>
 							</div>
@@ -29,7 +29,8 @@ class SignIn extends Component {
 							</div>*/}
 							<a type="submit" className="waves-effect waves-light btn light-green darken-4">Login</a>
 						</form>
-						<a href="/auth/google" className="sign-in-with-google waves-effect waves-light col s12 m6 l6 offset-m3 offset-l3 white">
+						<div className="divider"></div>
+						<a href="/auth/google" className="sign-in-with-google waves-effect waves-light col s12 m6 l6 offset-m3 offset-l3 grey lighten-3">
 							<div className="google-plus-icon-container valign-wrapper red darken-5">
 								<FaGooglePlus className="white-text" size={32} />
 							</div>
