910de3d1d471a7709743b4b26babe2d0cff57a83
Focus <Focus@nat-wireless-guest-reg-153-50.bu.edu>
2018-02-05 13:24:04 -0500

    adds 'Sign in with Google' link in SignIn component

--- client/src/components/SignIn/SignIn.js
+++ client/src/components/SignIn/SignIn.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FaGooglePlus from 'react-icons/lib/fa/google-plus';
 
 import './SignIn.css';
 
@@ -8,8 +9,8 @@ class SignIn extends Component {
 			<div className="sign-in-component">
 				<div className="container">
 					<h5 className="welcome-back center-align"> Welcome back! </h5>
-					<div className="local-sign-in-form-container row">
-						<form className="col s12 m6 l6 offset-m3 offset-l3 blue-grey lighten-5">
+					<div className="sign-in-form-container row">
+						<form className="local-sign-in col s12 m6 l6 offset-m3 offset-l3 blue-grey lighten-5">
 							<div className="row">
 								<div className="input-field col s12 m12 l12">
 									<input placeholder="jsmith@gmail.com" id="email" type="text" className="validate"/>
@@ -18,7 +19,7 @@ class SignIn extends Component {
 							</div>
 							<div className="row">
 								<div className="input-field col s12 m12 l12">
-									<input placeholder="******" id="password" type="text" className="validate"/>
+									<input placeholder="******" id="password" type="password" className="validate"/>
 									<label htmlFor="password">Password</label>
 						        </div>
 							</div>
@@ -28,6 +29,12 @@ class SignIn extends Component {
 							</div>*/}
 							<a type="submit" className="waves-effect waves-light btn light-green darken-4">Login</a>
 						</form>
+						<a href="/auth/google" className="sign-in-with-google waves-effect waves-light col s12 m6 l6 offset-m3 offset-l3 white">
+							<div className="google-plus-icon-container valign-wrapper red darken-5">
+								<FaGooglePlus className="white-text" size={32} />
+							</div>
+							<p> Sign in with Google </p>
+						</a>
 					</div>
 				</div>
 			</div>
