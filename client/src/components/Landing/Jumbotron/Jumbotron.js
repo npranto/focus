265cf06a4c08f2afff0dbd738a518a45aa4de2c7
Focus <Focus@Nazmuzs-MacBook-Air.local>
2018-02-06 00:22:02 -0500

    completes a draft of SignIn component

--- client/src/components/Landing/Jumbotron/Jumbotron.js
+++ client/src/components/Landing/Jumbotron/Jumbotron.js
@@ -9,9 +9,9 @@ class Jumbotron extends Component {
 				<h1 className="title"> Focus</h1>
 				<h3 className="subtitle"> Get your work <br/> done today! </h3>
 				<div className="actions">
-					<a className="waves-effect waves-light btn deep-orange darken-4"> Login </a>
+					<a href="/sign-in" className="waves-effect waves-light btn deep-orange darken-4"> Login </a>
 					<br />
-					<a className="waves-effect waves-light btn green darken-3"> Create New Account </a>
+					<a href="/sign-up" className="waves-effect waves-light btn green darken-3"> Create New Account </a>
 				</div>
 			</div>	
 		)
