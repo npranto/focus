a2181ecc978937bc666355d2f49d42fb5d9305ce
Focus <Focus@Nazmuzs-MacBook-Air.local>
2018-02-12 16:41:17 -0500

    adds multer package to upload profile pictures

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const authRoutes = require('./authRoutes');
 const userRoutes = require('./userRoutes');
 const reviewRoutes = require('./reviewRoutes');
 const taskRoutes = require('./taskRoutes');
+const uploadRoutes = require('./uploadRoutes');
 
 const routes = (app) => {
 	app.use('/auth', authRoutes);
 	app.use('/api/users', userRoutes);
 	app.use('/api/reviews', reviewRoutes);
 	app.use('/api/tasks', taskRoutes);
+	app.use('/api/uploads', uploadRoutes)
 }
 
 module.exports = routes;
