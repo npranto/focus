fab75e4d92b8d6ac32488c1c446f6b9bcf27e120
Focus <Focus@nat-wireless-guest-reg-153-50.bu.edu>
2018-02-28 14:53:15 -0500

    completes layout, state and actions for Jumbotron component

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,21 +1,42 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import Media from "react-media";
 
+import YoungManWorkingAtDeskXs from './../../assets/young-man-working-at-desk-xs.jpeg';
 import './Jumbotron.css';
 
 class Jumbotron extends Component {
 	render() {
 		return (
-			<div className="jumbotron">
-				<div className="jumbotron-background-picture">
-					<h1 className="title"> Focus </h1>
-					<h5 className="subtitle"> Get your work <br /> done today! </h5>
-					<div className="jumbotron-actions">
-						<Link to="/sign-in" class="waves-effect waves-light btn red darken-4"> Login </Link>
-						<br />
-						<Link to="/sign-up" class="waves-effect waves-light btn green darken-4"> Sign Up </Link>
-					</div>
-				</div>
+			<div className="jumbotron blue-grey lighten-5">
+				<Media query="(min-width: 500px)">
+		          {matches =>
+		            matches ? (
+		                <div className="jumbotron-background-picture large-screen">
+							<h1 className="title"> Focus </h1>
+							<h5 className="subtitle"> Get your work <br /> done today! </h5>
+							<div className="jumbotron-actions">
+								<Link to="/sign-in" className="waves-effect waves-light btn red darken-4"> Login </Link>
+								<br />
+								<Link to="/sign-up" className="waves-effect waves-light btn green darken-4"> Sign Up </Link>
+							</div>
+						</div>
+		            ) : (
+		            	<div className="small-screen">
+		            		<h2 className="title center-align"> Focus </h2>
+							<div className="jumbotron-picture">
+								<img src={YoungManWorkingAtDeskXs} alt="Young Man Working at Desk" />
+							</div>
+							<h5 className="subtitle center-align"> Get your work <br /> done today! </h5>
+							<div className="jumbotron-actions center-align">
+								<Link to="/sign-in" className="waves-effect waves-light btn red darken-4"> Login </Link>
+								<br />
+								<Link to="/sign-up" className="waves-effect waves-light btn green darken-4"> Sign Up </Link>
+							</div>
+						</div>
+		            )
+		          }
+		        </Media>
 			</div>
 		)
 	}
