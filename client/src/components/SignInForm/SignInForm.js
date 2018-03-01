27d21ed509c997e78754f668821c5fc01c93fb11
Focus <Focus@nat-wireless-guest-reg-153-50.bu.edu>
2018-03-01 12:55:36 -0500

    working on submutting login form

--- client/src/components/SignInForm/SignInForm.js
+++ client/src/components/SignInForm/SignInForm.js
@@ -1,26 +1,45 @@
 import React, {Component} from 'react';
+import { Field, reduxForm } from 'redux-form'
 
+import SignInFormInput from './../SignInFormInput/SignInFormInput';
 import './SignInForm.css';
 
 class SignInForm extends Component {
+
+	onSignInFormSubmit(values) {
+
+	}
+
 	render() {
+		const {pristine, submitting } = this.props;
+
 		return (
 			<div className="sign-in-form">
-				<form className="col s12 yellow lighten-5">
+				<form className="col s12 yellow lighten-5" onSubmit={this.onSignInFormSubmit}>
 					<div className="row">
-						<div className="input-field col s12">
-							<input placeholder="" id="email" type="email" className="validate" />
-							<label htmlFor="email"> Email </label>
-						</div>
+						<Field
+				            key={0}
+							name="email"
+							placeholder=""
+							id="email"
+							type="email"
+							htmlFor="email"
+							label="Email"
+							component={SignInFormInput} />
 					</div>
 					<div className="row">
-						<div className="input-field col s12">
-							<input placeholder="" id="password" type="password" className="validate" />
-							<label htmlFor="password"> Password </label>
-						</div>
+						<Field
+				            key={1}
+							name="password"
+							placeholder=""
+							id="password"
+							type="password"
+							htmlFor="password"
+							label="Password"
+							component={SignInFormInput} />
 					</div>
 					<div className="row">
-						<button type="submit" className="login-button col s12 waves-effect waves-light btn green darken-2"> Login </button>
+						<button type="submit" disabled={pristine || submitting} className="login-button col s12 waves-effect waves-light btn green darken-2"> Login </button>
 			    	</div>
 			    </form>
 			</div>
@@ -28,4 +47,26 @@ class SignInForm extends Component {
 	}
 }
 
-export default SignInForm;
+const validate = (form) => {
+	let errors = {};
+
+	if (!form.email) {
+		errors.email = 'Please enter your email';
+	} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(form.email)) {
+		errors.email = 'Please enter a valid email'
+	} 
+
+
+	if (!form.password) {
+		errors.password = 'Please enter your password';
+	} else if (form.password.length < 6) {
+		errors.password = 'Your password must be at least 6 characters long'
+	} 
+
+	return errors;
+}
+
+export default reduxForm({
+	form: 'signInForm',
+	validate
+})(SignInForm);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,13 @@
 import {combineReducers} from 'redux';
+import { reducer as formReducer } from 'redux-form'
+
 import authReducer from './authReducer';
 import componentsReducer from './componentsReducer';
 
 const reducers = combineReducers({
 	components: componentsReducer,
-	auth: authReducer
+	auth: authReducer,
+	form: formReducer
 })
 
 export default reducers;
