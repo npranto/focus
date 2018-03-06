3f2dc78924c8a1d63ced439b7a5deb5560ab1439
Focus <Focus@nat-wireless-guest-reg-153-50.bu.edu>
2018-03-06 17:28:08 -0500

    adds TermsOfConditions and PrivacyPolicy components and add link to those two components in SignUpForm component

---
+++ client/src/components/PrivacyPolicy/PrivacyPolicy.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import './PrivacyPolicy.css'
+
+class PrivacyPolicy extends Component {
+	render() {
+		return (
+			<div className="privacy-policy">
+				<h3 className="center-align"> Privacy Policy </h3>
+			</div>
+		)
+	}
+}
+
+export default PrivacyPolicy;
---
+++ client/src/components/TermsOfConditions/TermsOfConditions.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import './TermsOfConditions.css'
+
+class TermsOfConditions extends Component {
+	render() {
+		return (
+			<div className="terms-of-conditions">
+				<h3 className="center-align"> Terms of Conditions </h3>
+			</div>
+		)
+	}
+}
+
+export default TermsOfConditions;
