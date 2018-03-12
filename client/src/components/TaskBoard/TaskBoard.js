ee0ebfc4154e17f405f2573b7b30d192573c21f3
Focus <Focus@nat-wireless-guest-reg-153-50.bu.edu>
2018-03-12 11:47:23 -0400

    adding layout for TaskViewSwitcher component; connects redux state to TaskBoard to properly render content based on active view mode

--- client/src/components/TaskBoard/TaskBoard.js
+++ client/src/components/TaskBoard/TaskBoard.js
@@ -1,17 +1,24 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 import Tasks from './../Tasks/Tasks';
+import TaskViewSwitcher from './../TaskViewSwitcher/TaskViewSwitcher';
 import './TaskBoard.css';
 
 class TaskBoard extends Component {
 	render() {
 		const {tasks} = this.props;
+		const {viewModes} = this.props.components.taskBoard;
+		const activeViewMode = viewModes.find((viewMode) => {
+			return viewMode.active;
+		});
+
 		return (
 			<div className="task-board">
 				<div className="header">
-					<h4> Tasks </h4>
+					<h4> {activeViewMode.title} </h4>
 					<div className="task-view-switcher-container">
-						TaskViewSwitcher
+						<TaskViewSwitcher />
 					</div>
 				</div>
 				<div className="tasks">
@@ -22,4 +29,10 @@ class TaskBoard extends Component {
 	}
 }
 
-export default TaskBoard;
+const mapStateToProps = (state) => {
+	return {
+		components: state.components
+	}
+}
+
+export default connect(mapStateToProps)(TaskBoard);
