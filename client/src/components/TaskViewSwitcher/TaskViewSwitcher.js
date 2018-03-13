3ba57b551195ea5e3493ace1208eb5edcc1cab05
Focus <Focus@Nazmuzs-MacBook-Air.local>
2018-03-13 13:44:04 -0400

    adds layout for TrackProgress component

--- client/src/components/TaskViewSwitcher/TaskViewSwitcher.js
+++ client/src/components/TaskViewSwitcher/TaskViewSwitcher.js
@@ -2,18 +2,19 @@ import React, {Component} from 'react';
 import MdFormatListBulleted from 'react-icons/lib/md/format-list-bulleted';
 import TiCalender from 'react-icons/lib/ti/calendar';
 import MdBorderInner from 'react-icons/lib/md/border-inner';
+import Media from 'react-media';
 
 import './TaskViewSwitcher.css';
 
 class TaskViewSwitcher extends Component {
 
-	renderIconBasedOnViewModeId(id) {
+	renderIconBasedOnViewModeId(id, iconSize) {
 		if (id === 'tasks') {
-			return <MdFormatListBulleted size={28} />;
+			return <MdFormatListBulleted size={iconSize} />;
 		} else if (id === 'calendar') {
-			return <TiCalender size={28} />
+			return <TiCalender size={iconSize} />
 		} else if (id === 'prioritize-mode') {
-			return <MdBorderInner size={28} />
+			return <MdBorderInner size={iconSize} />
 		}
 	}
 
@@ -21,7 +22,14 @@ class TaskViewSwitcher extends Component {
 		return viewModes.map((viewMode, index) => {
 			return (
 				<a key={index} onClick={() => this.props.onChangeViewMode(viewMode.id)} className={`${viewMode.id}-icon icon-container tooltipped ${viewMode.active ? 'active' : ''}`} data-position="bottom" data-delay="50" data-tooltip={viewMode.title}>
-					{this.renderIconBasedOnViewModeId(viewMode.id)}
+                    <Media query="(min-width: 1000px)">
+                        {matches =>
+                            matches
+                                ? this.renderIconBasedOnViewModeId(viewMode.id, 28)
+                                : this.renderIconBasedOnViewModeId(viewMode.id, 24)
+
+                        }
+                    </Media>
 				</a>
 			)
 		})
